0c53c662b386f4c78b08392511c9f7a0a261436c
lloydblake <lloyd.blake@footballndex.co.uk>
2020-02-07 14:48:49 +0000

    Updates

---
+++ precache-manifest.567bd4f16f0337b24ad6bd3b298f9d92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cafe3fccd722d59477a0ff1191c1941c",
+    "url": "/react-app-test/index.html"
+  },
+  {
+    "revision": "376edf21ef2eae6c22f7",
+    "url": "/react-app-test/static/css/main.95a5606d.chunk.css"
+  },
+  {
+    "revision": "0cf76846103cb77beffe",
+    "url": "/react-app-test/static/js/2.0059f0b2.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/react-app-test/static/js/2.0059f0b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "376edf21ef2eae6c22f7",
+    "url": "/react-app-test/static/js/main.95df9d95.chunk.js"
+  },
+  {
+    "revision": "99320fc753d72352796e",
+    "url": "/react-app-test/static/js/runtime-main.02a4c29c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
